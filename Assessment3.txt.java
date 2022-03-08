4ef17d5398f81f523be18341c2a065c57f629d09
ShivajiKurai <98843436+ShivajiKurai@users.noreply.github.com>
2022-03-08 22:24:45 +0530

    Add files via upload

---
+++ Assessment3.txt.java
@@ -0,0 +1,24 @@
+import $ from "jquery";
+function toggle(button)
+
+{
+    
+   switch(button.value)
+    
+{
+       
+    case "ON":
+           
+    button.value = "OFF";
+          
+    break;
+        
+    case "OFF":
+            
+    button.value = "ON";
+           
+    break;
+    
+}
+
+}
